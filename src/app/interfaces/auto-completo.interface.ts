a3f77349e8923e7e50be5169ae47d48a4fcf8e09
federicoPatuto <federipatu@gmail.com>
2025-11-20 15:58:15 -0300

    Update auto-completo.interface.ts

--- src/app/interfaces/auto-completo.interface.ts
+++ src/app/interfaces/auto-completo.interface.ts
@@ -3,6 +3,7 @@ import { Concesionaria } from "./concesionaria.interface";
 import { Marca } from "./marca.interface";
 import { Modelo } from "./modelo.interface";
 
+//Interfaz enriquecida. Se construye a partir de la combinación de las interfaces "primitivas" que mapean desde JSON, con objetivo de volver más eficiente el manejo de datos en tiempo de ejecución.
 export interface AutoCompleto {
     auto: Auto;
     imagen: string & any [] | string[];
